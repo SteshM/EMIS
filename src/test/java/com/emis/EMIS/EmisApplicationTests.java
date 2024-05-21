c7f34c02cb9509638f7cb315991c629324cdd34d
stella <mbithestella0@gmail.com>
2024-05-21 12:02:28 +0300

    Initial commit

---
+++ src/test/java/com/emis/EMIS/EmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emis.EMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
