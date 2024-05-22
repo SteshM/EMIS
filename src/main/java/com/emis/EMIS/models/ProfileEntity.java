d49d3f0f660cc149662d5fe66b7e4c27209cbc91
stella <mbithestella0@gmail.com>
2024-05-22 09:24:04 +0300

    Created the profile entity

---
+++ src/main/java/com/emis/EMIS/models/ProfileEntity.java
@@ -0,0 +1,20 @@
+package com.emis.EMIS.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "profiles")
+public class ProfileEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int profileId;
+    private String username;
+    private String password;
+
+}
