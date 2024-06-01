5d04a00e36348627f7c927b2e745dab179aace0c
stella <mbithestella0@gmail.com>
2024-06-01 13:28:06 +0300

    Created the school entity

--- src/main/java/com/emis/EMIS/models/SchoolsEntity.java
+++ src/main/java/com/emis/EMIS/models/SchoolsEntity.java
@@ -1,3 +1,26 @@
 package com.emis.EMIS.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "schools")
 public class SchoolsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int schoolId;
+    private String schoolName;
+    private String schoolType;
+    private String emailAddress;
+    private String postalAddress;
+    private String postalCode;
+    private String contact;
+    private String moeRegistrationNo;
+    private String county;
+    private String subCounty;
+    private String location;
+
 }
