fd5a28ee8ca66b563d865a27af6919709a67ba9e
stella <mbithestella0@gmail.com>
2024-06-06 08:56:19 +0300

    created other admin's Repo

---
+++ src/main/java/com/emis/EMIS/repositories/OtherAdminsRepo.java
@@ -0,0 +1,7 @@
+package com.emis.EMIS.repositories;
+
+import com.emis.EMIS.models.OtherAdminEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OtherAdminsRepo extends JpaRepository<OtherAdminEntity,Integer> {
+}
