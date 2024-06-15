81e197644b3da6167df22f86078042e929c832e1
stella <mbithestella0@gmail.com>
2024-06-15 17:17:43 +0300

    validated fields in ActivateAccDTO

--- src/main/java/com/emis/EMIS/wrappers/requestDTOs/ActivateAccDTO.java
+++ src/main/java/com/emis/EMIS/wrappers/requestDTOs/ActivateAccDTO.java
@@ -1,5 +1,9 @@
 package com.emis.EMIS.wrappers.requestDTOs;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +11,11 @@ import lombok.Setter;
 @Getter
 
 public class ActivateAccDTO {
+    @NotEmpty(message = "An otp is required!")
     private String otp;
+    @NotBlank(message = "Email is mandatory!")
     private String email;
+    @NotNull(message = "This field cannot be null")
+    @Size(min = 5,max=8)
     private String password;
 }
