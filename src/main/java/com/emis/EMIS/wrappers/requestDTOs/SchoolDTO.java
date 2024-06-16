d8c07df85fedc3029586432bfa6cf3fea61567b4
stella <mbithestella0@gmail.com>
2024-06-16 19:27:23 +0300

    updated the school dto

--- src/main/java/com/emis/EMIS/wrappers/requestDTOs/SchoolDTO.java
+++ src/main/java/com/emis/EMIS/wrappers/requestDTOs/SchoolDTO.java
@@ -10,23 +10,32 @@ import lombok.Data;
 public class SchoolDTO {
     @NotNull(message = "SchoolName is required")
     private String schoolName;
+
     @NotBlank(message = "School type cannot be blank")
     private String schoolType;
+
     @Email
     @Column(unique = true)
     private String emailAddress;
+
     @NotNull(message = "This field cannot be null")
     private String postalAddress;
+
     @NotNull(message = "This field cannot be null")
     private String postalCode;
+
     @NotBlank(message = "This field cannot be blank")
     private String contact;
+
     @NotNull(message = "This field cannot be null")
     private String moeRegistrationNo;
+
     @NotBlank(message = "This field cannot be blank")
     private String county;
+
     @NotNull(message = "This field cannot be null")
     private String subCounty;
+
     @NotBlank(message = "This field cannot be blank")
     private String location;
 }
