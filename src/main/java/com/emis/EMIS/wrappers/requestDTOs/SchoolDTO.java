c4b81fa8b6b0bd45c9891bd481a84ded90aaa256
stella <mbithestella0@gmail.com>
2024-06-16 19:25:39 +0300

    validated fields in the user dto and school dto

--- src/main/java/com/emis/EMIS/wrappers/requestDTOs/SchoolDTO.java
+++ src/main/java/com/emis/EMIS/wrappers/requestDTOs/SchoolDTO.java
@@ -2,20 +2,31 @@ package com.emis.EMIS.wrappers.requestDTOs;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class SchoolDTO {
+    @NotNull(message = "SchoolName is required")
     private String schoolName;
+    @NotBlank(message = "School type cannot be blank")
     private String schoolType;
     @Email
     @Column(unique = true)
     private String emailAddress;
+    @NotNull(message = "This field cannot be null")
     private String postalAddress;
+    @NotNull(message = "This field cannot be null")
     private String postalCode;
+    @NotBlank(message = "This field cannot be blank")
     private String contact;
+    @NotNull(message = "This field cannot be null")
     private String moeRegistrationNo;
+    @NotBlank(message = "This field cannot be blank")
     private String county;
+    @NotNull(message = "This field cannot be null")
     private String subCounty;
+    @NotBlank(message = "This field cannot be blank")
     private String location;
 }
--- src/main/java/com/emis/EMIS/wrappers/requestDTOs/UserDTO.java
+++ src/main/java/com/emis/EMIS/wrappers/requestDTOs/UserDTO.java
@@ -2,6 +2,9 @@ package com.emis.EMIS.wrappers.requestDTOs;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,36 +12,92 @@ import java.util.Date;
 
 @Data
 public class UserDTO {
+    @NotBlank(message = "First name is required!")
     private String firstName;
+
+    @NotBlank(message = "Middle name is required!")
     private String middleName;
+
+    @NotBlank(message = "Last name is required!")
     private String lastName;
+
     @Email
     @Column(unique = true)
+    @NotBlank(message = " email is required!")
     private String email;
+
+    @NotNull(message = "This field cannot be null")
     private String nationalId;
+
+    @NotNull(message = "This field cannot be null")
     private String phoneNo;
+
     private String password;
+
+    @NotNull(message = "This field cannot be null")
     private String agencyName;
+
+    @NotNull(message = "This field cannot be null")
     private String firmName;
+
+    @NotNull(message = "This field cannot be null")
     private String emergencyContact;
+
+    @NotNull(message = "This field cannot be null")
     private String businessContact;
+
+    @NotNull(message = "This field cannot be null")
     private String businessEmail;
+
+    @NotNull(message = "This field cannot be null")
     private String businessPhone;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private Date agreementStartDate;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private Date agreementEndDate;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private String contractDetails;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private String adminRole;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private String department;
+
+    @NotEmpty(message = "This field cannot be empty!")
     private String officePhone;
+
+    @NotBlank(message = "This field cannot be blank!")
     private String tscNo;
+
+    @NotBlank(message = "This field is mandatory!")
     private String employmentNo;
+
+    @NotBlank(message = "This field is required!")
     private String officePhoneNo;
+
+    @NotBlank(message = "This field is required!")
     private String registrationNo;
+
+    @NotBlank(message = "This field is mandatory!")
     private String gender;
+
+    @NotBlank(message = "This field is mandatory!")
     private String nationality;
+
+    @NotBlank(message = "nationality is required!")
     private String dateOfBirth;
+
+    @NotBlank(message = " date of birth is required!")
     private int profileId;
+
+    @NotBlank(message = "profileId is required!")
     private String relationship;
+
+    @NotBlank(message = " occupation is required!")
     private String occupation;
 
 
