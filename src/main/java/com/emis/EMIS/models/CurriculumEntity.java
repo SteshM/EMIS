5b8379eae7f5f3ed81452825bf4980ff6d7f2ff3
stella <mbithestella0@gmail.com>
2024-06-19 12:21:18 +0300

    Created CurriculumEntity

---
+++ src/main/java/com/emis/EMIS/models/CurriculumEntity.java
@@ -0,0 +1,14 @@
+package com.emis.EMIS.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "curriculum")
+public class CurriculumEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int curriculumId;
+    private String curriculum;
+}
