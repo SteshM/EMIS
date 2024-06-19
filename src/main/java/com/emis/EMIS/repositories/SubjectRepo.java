3c2b427307d861f6dedca758e061a816a2ea6324
stella <mbithestella0@gmail.com>
2024-06-19 12:18:57 +0300

    Created subject  repo

---
+++ src/main/java/com/emis/EMIS/repositories/SubjectRepo.java
@@ -0,0 +1,7 @@
+package com.emis.EMIS.repositories;
+
+import com.emis.EMIS.models.SubjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepo extends JpaRepository<SubjectEntity,Integer> {
+}
