2b06fc1c5e5b08a1a3e238f94af8b0b00a9bc744
stella <mbithestella0@gmail.com>
2024-06-19 12:04:03 +0300

    Created grade entity

---
+++ src/main/java/com/emis/EMIS/models/GradeEntity.java
@@ -0,0 +1,14 @@
+package com.emis.EMIS.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "grades")
+public class GradeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int gradeId;
+    private String grade;
+}
