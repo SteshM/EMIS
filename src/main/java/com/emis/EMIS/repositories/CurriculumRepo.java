2efb919f11abe254a2bc52724d880eb8e319e0a6
stella <mbithestella0@gmail.com>
2024-06-19 12:24:08 +0300

    Created Curriculum request dto

--- src/main/java/com/emis/EMIS/repositories/CurriculumRepo.java
+++ src/main/java/com/emis/EMIS/repositories/CurriculumRepo.java
@@ -1,7 +1,6 @@
 package com.emis.EMIS.repositories;
 
 import com.emis.EMIS.models.CurriculumEntity;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CurriculumRepo  extends JpaRepository<CurriculumEntity,Integer> {
