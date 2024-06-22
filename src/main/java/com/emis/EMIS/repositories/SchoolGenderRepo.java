b97a77b47983a3b0d2f4b845753a752ee8d64666
stella <mbithestella0@gmail.com>
2024-06-22 17:14:35 +0300

    Created  school gender repo

---
+++ src/main/java/com/emis/EMIS/repositories/SchoolGenderRepo.java
@@ -0,0 +1,7 @@
+package com.emis.EMIS.repositories;
+
+import com.emis.EMIS.models.SchoolGender;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchoolGenderRepo extends JpaRepository<SchoolGender,Integer> {
+}
