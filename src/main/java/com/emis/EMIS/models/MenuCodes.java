d7569b565a655b3acbc63818bec98bf855185c33
stella <mbithestella0@gmail.com>
2024-06-24 17:44:53 +0300

    Created menu codes entity and repo

---
+++ src/main/java/com/emis/EMIS/models/MenuCodes.java
@@ -0,0 +1,26 @@
+package com.emis.EMIS.models;
+
+import com.emis.EMIS.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "menuCodes")
+public class MenuCodes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int menuCodeId;
+    private String name;
+    private String remarks;
+    private boolean required = true;
+    @Column(name = "records_required")
+    private Integer recordsRequired;
+    private Integer completionPercentage;
+    private String createdBy;
+    private String  updatedBy;
+    private Status status;
+
+}
---
+++ src/main/java/com/emis/EMIS/repositories/MenuCodesRepo.java
@@ -0,0 +1,7 @@
+package com.emis.EMIS.repositories;
+
+import com.emis.EMIS.models.MenuCodes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuCodesRepo extends JpaRepository<MenuCodes,Integer> {
+}
