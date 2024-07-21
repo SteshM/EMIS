fa4c2697eb12832336b5d9c6b7a3b84bff584b0b
stella <mbithestella0@gmail.com>
2024-07-22 01:39:22 +0300

    uploaded files

---
+++ src/main/java/com/emis/EMIS/wrappers/responseDTOs/SubjectTeacherDTO.java
@@ -0,0 +1,12 @@
+package com.emis.EMIS.wrappers.responseDTOs;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class SubjectTeacherDTO {
+    private String subject;
+}
